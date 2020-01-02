d389772dec876df763157147db074875ae206eff
Jamie Redmond <4458771+jcredmond@users.noreply.github.com>
2020-01-02 16:33:21 +0100

    Update copyrights

--- Source/FaceFX/Private/Audio/FaceFXAudioImplWwise.h
+++ Source/FaceFX/Private/Audio/FaceFXAudioImplWwise.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
 The MIT License (MIT)
-Copyright (c) 2015-2019 OC3 Entertainment, Inc. All rights reserved.
+Copyright (c) 2015-2020 OC3 Entertainment, Inc. All rights reserved.
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
